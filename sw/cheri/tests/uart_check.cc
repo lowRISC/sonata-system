88e83ac02fb82dc3e17f301b9ea39374e3099f23
Hugo McNally <hugo.mcnally@gmail.com>
2024-05-13 13:15:46 +0100

    Moved UART check over to upstream driver

--- sw/cheri/tests/uart_check.cc
+++ sw/cheri/tests/uart_check.cc
@@ -2,61 +2,18 @@
 #define CHERIOT_NO_NEW_DELETE
 #define CHERIOT_PLATFORM_CUSTOM_UART
 
+#include "../../common/defs.h"
 #include <cheri.hh>
 #include <platform-uart.hh>
 #include <stdint.h>
 
 using namespace CHERI;
 
-#define GPIO_VALUE  (0xFFFFFFFF)
-#define CPU_FREQ_HZ (30'000'000)
-#define BAUD_RATE   (   115'200)
-
-/**
- * OpenTitan UART
- */
-class OpenTitanUart
-{
-  typedef uint32_t RegisterType;
-
- [[maybe_unused]] RegisterType intrState;
- [[maybe_unused]] RegisterType intrEnable;
- [[maybe_unused]] RegisterType intrTest;
- [[maybe_unused]] RegisterType alertTest;
- [[maybe_unused]] RegisterType ctrl;
- [[maybe_unused]] RegisterType status;
- [[maybe_unused]] RegisterType rData;
- RegisterType wData;
- [[maybe_unused]] RegisterType fifoCtrl;
- RegisterType fifoStatus;
- [[maybe_unused]] RegisterType ovrd;
- [[maybe_unused]] RegisterType val;
- [[maybe_unused]] RegisterType timeoutCtrl;
-
-  public:
-  void init() volatile
-  {
-    // NCO = 2^20 * baud rate / cpu frequency
-    const uint32_t nco = (((uint64_t) BAUD_RATE << 20) / CPU_FREQ_HZ);
-    // Set the baud rate and enable transmit & receive
-    ctrl = (nco << 16) | 0b11;
-  };
-
-  bool can_write() volatile
-  {
-    return (fifoStatus & 0xff) < 32;
-  };
-
-  /**
-   * Write one byte, blocking until the byte is written.
-   */
-  void blocking_write(uint8_t byte) volatile
-  {
-    while (!can_write()) {}
-    wData = byte;
+void write(volatile OpenTitanUart<>* uart, const char* str) {
+  for (; *str != '\0'; ++str) {
+    uart->blocking_write(*str);
   }
-};
-
+}
 
 /**
  * C++ entry point for the loader.  This is called from assembly, with the
@@ -68,19 +25,16 @@ extern "C" void rom_loader_entry(void *rwRoot)
   Capability<void> root{rwRoot};
 
   // Create a bounded capability to the UART
-  Capability<volatile OpenTitanUart> uart = root.cast<volatile OpenTitanUart>();
-  uart.address() = 0x80100000;
-  uart.bounds()  = 0x34;
+  Capability<volatile OpenTitanUart<>> uart = root.cast<volatile OpenTitanUart<>>();
+  uart.address() = UART_ADDRESS;
+  uart.bounds()  = UART_BOUNDS;
 
-  uart->init();
-  uart->blocking_write('h');
-  uart->blocking_write('i');
-  uart->blocking_write('\r');
-  uart->blocking_write('\n');
+  uart->init(BAUD_RATE);
+  write(uart, "Hello There!\r\n");
+
+  char ch = '\n';
   while (true) {
-    uart->blocking_write('h');
-    uart->blocking_write('i');
-    uart->blocking_write('\r');
-    uart->blocking_write('\n');
+    ch = uart->blocking_read();
+    uart->blocking_write(ch);
   }
 }
