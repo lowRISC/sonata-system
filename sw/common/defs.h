77bce4aaf6b94ba666f12822323f44fe6dd3775e
Hugo McNally <hugo.mcnally@gmail.com>
2024-05-13 13:15:46 +0100

    Moved UART check over to upstream driver

---
+++ sw/common/defs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define CPU_TIMER_HZ (30'000'000)
+#define BAUD_RATE    (   115'200)
+
+#define GPIO_ADDRESS (0x8000'0000)
+#define GPIO_BOUNDS  (0x0000'0020)
+
+#define UART_ADDRESS (0x8010'0000)
+#define UART_BOUNDS  (0x0000'0034)
