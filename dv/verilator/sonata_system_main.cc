0393387fce990d923ebc358deb76061f6572cdcb
Hugo McNally <hugo.mcnally@gmail.com>
2024-05-14 09:50:44 +0100

    Fixed simulator's path to memory.

--- dv/verilator/sonata_system_main.cc
+++ dv/verilator/sonata_system_main.cc
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv) {
   SonataSystem sonata_system(
-      "TOP.top_verilator.u_sonata_system.u_sram_top.u_ram.u_ram.gen_generic.u_impl_generic",
+      "TOP.top_verilator.u_sonata_system.u_sram_top.u_ram.gen_generic.u_impl_generic",
       1024 * 1024);
 
   return sonata_system.Main(argc, argv);
