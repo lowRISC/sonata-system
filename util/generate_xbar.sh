13554deb32fc29dc71c41185da47e1c41c8945d1
Greg Chadwick <gac@lowrisc.org>
2024-08-07 20:42:10 +0100

    Add new crossbar for Ibex instruction fetch
    Includes hyperram port for hyperram integration

--- util/generate_xbar.sh
+++ util/generate_xbar.sh
@@ -1,5 +1,6 @@
 mkdir -p rtl/bus
 vendor/lowrisc_ip/util/tlgen.py -t data/xbar_main.hjson -o rtl/bus
+vendor/lowrisc_ip/util/tlgen.py -t data/xbar_ifetch.hjson -o rtl/bus
 rm -r rtl/bus/data
 rm -r rtl/bus/dv
 mv rtl/bus/rtl/autogen/*.sv rtl/bus
