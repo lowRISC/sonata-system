dd70b3b8fe323b51816eb3c0ce004538e7a91db5
Douglas Reis <doreis@lowrisc.org>
2024-08-23 16:02:41 +0100

    [common] Add a helper function for console features
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

---
+++ sw/cheri/common/console-utils.hh
@@ -0,0 +1,29 @@
+#pragma once
+#include "../../common/defs.h"
+#include "uart-utils.hh"
+#include <platform-uart.hh>
+
+#define CC_BOLD "1"
+#define CC_RED "31"
+#define CC_GREEN "32"
+#define CC_RESET "0"
+
+
+[[maybe_unused]] static void set_console_mode(volatile OpenTitanUart *uart,
+                      const char *cc) {
+  write_str(uart, "\x1b[");
+  write_str(uart, cc);
+  write_str(uart, "m");
+}
+
+[[maybe_unused]] static void write_test_result(volatile OpenTitanUart *uart,
+                       int failures) {
+  if (failures == 0) {
+    set_console_mode(uart, CC_GREEN);
+    write_str(uart, "PASS!\n");
+  } else {
+    set_console_mode(uart, CC_RED);
+    write_str(uart, "FAIL!\n");
+  }
+  set_console_mode(uart, CC_RESET);
+}
