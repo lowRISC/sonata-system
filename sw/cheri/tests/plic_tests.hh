bb777b1dc462f371cbff0f80d9cd8af230da3c7a
Douglas Reis <doreis@lowrisc.org>
2024-09-10 17:07:46 +0100

    [test, plic] Test the i2c interrupts

--- sw/cheri/tests/plic_tests.hh
+++ sw/cheri/tests/plic_tests.hh
@@ -88,6 +88,59 @@ struct PlicTest {
     }
   }
 
+  void i2c_test(size_t i2c_instance) {
+    instance = i2c_instance;
+    constexpr uint32_t instanceOffset =
+        PLIC::Interrupts::I2c1FormatFifoThreshold - PLIC::Interrupts::I2c0FormatFifoThreshold;
+
+    struct i2c_irq {
+      OpenTitanI2cInterrupt id;
+      bool can_clear;
+    };
+    constexpr std::array<std::pair<i2c_irq, PLIC::Interrupts>, 15> i2cMap = {{
+        {{OpenTitanI2cInterrupt::ReceiveOverflow, true}, PLIC::Interrupts::I2c0ReceiveFifoOverflow},
+        {{OpenTitanI2cInterrupt::Nak, true}, PLIC::Interrupts::I2c0ReceiveNack},
+        {{OpenTitanI2cInterrupt::SclInterference, true}, PLIC::Interrupts::I2c0SclInterference},
+        {{OpenTitanI2cInterrupt::SdaInterference, true}, PLIC::Interrupts::I2c0SdaInterference},
+        {{OpenTitanI2cInterrupt::StretchTimeout, true}, PLIC::Interrupts::I2c0StretchTimeout},
+        {{OpenTitanI2cInterrupt::SdaUnstable, true}, PLIC::Interrupts::I2c0SdaUnstable},
+        {{OpenTitanI2cInterrupt::CommandComplete, true}, PLIC::Interrupts::I2c0CommandComplete},
+        {{OpenTitanI2cInterrupt::UnexpectedStop, true}, PLIC::Interrupts::I2c0UnexpectedStop},
+        {{OpenTitanI2cInterrupt::HostTimeout, true}, PLIC::Interrupts::I2c0HostTimeout},
+
+        {{OpenTitanI2cInterrupt::TransmitStretch, false}, PLIC::Interrupts::I2c0TransmitStretch},
+        {{OpenTitanI2cInterrupt::AcquiredFull, false}, PLIC::Interrupts::I2c0AcquireFifoFull},
+        {{OpenTitanI2cInterrupt::TransmitThreshold, false}, PLIC::Interrupts::I2c0TransmitThreshold},
+        {{OpenTitanI2cInterrupt::FormatThreshold, false}, PLIC::Interrupts::I2c0FormatFifoThreshold},
+        {{OpenTitanI2cInterrupt::ReceiveThreshold, false}, PLIC::Interrupts::I2c0ReceiveFifoThreshold},
+        {{OpenTitanI2cInterrupt::AcquiredThreshold, false}, PLIC::Interrupts::I2c0AcquireFifoThreshold},
+    }};
+
+    auto i2c = i2c_ptr(root, instance);
+    write_str(console, "testing i2c: ");
+    write_hex8b(console, instance);
+    write_str(console, "\r\n");
+    for (size_t i = 0; i < i2cMap.size(); ++i) {
+      ip_irq_id        = static_cast<uint32_t>(i2cMap[i].first.id);
+      is_irq_clearable = i2cMap[i].first.can_clear;
+      plic_irq_id      = static_cast<PLIC::Interrupts>(i2cMap[i].second + (instance * instanceOffset));
+
+      i2c->interrupt_enable(i2cMap[i].first.id);
+      irq_handler = [](PlicTest *plic_test, PLIC::Interrupts irq) {
+        plic_test->log(irq);
+        plic_test->error_count += (irq != plic_test->plic_irq_id);
+        auto i2c = i2c_ptr(plic_test->root, plic_test->instance);
+        if (plic_test->is_irq_clearable) {
+          i2c->interruptState = 0x1 << plic_test->ip_irq_id;
+        } else {
+          i2c->interrupt_disable(static_cast<OpenTitanI2cInterrupt>(plic_test->ip_irq_id));
+        }
+      };
+      i2c->interruptTest = 0x01 << ip_irq_id;
+      wfi();
+    }
+  }
+
   inline void wfi() {
     ASM::Ibex::global_interrupt_set(true);
     ASM::Ibex::global_interrupt_set(false);
@@ -118,6 +171,8 @@ struct PlicTest {
     ASM::Ibex::external_interrupt_set(true);
     uart_test(0);
     uart_test(1);
+    i2c_test(0);
+    i2c_test(1);
     return error_count == 0;
   }
 };
