7da19c93e70cfbcc7c4bbdc5a5d68b79f4d3241c
Douglas Reis <doreis@lowrisc.org>
2024-09-25 16:51:40 +0100

    [nix] move tests to a module
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- flake.nix
+++ flake.nix
@@ -165,39 +165,15 @@
         };
       };
 
-      tests-fpga = pkgs.writeShellApplication {
-        name = "tests-fpga";
-        runtimeInputs = [pythonEnv];
-        text = ''
-          set +u
-          if [ -z "$1" ]; then
-            echo "Please provide the tty device location (e.g. /dev/ttyUSB2)" \
-              "as the first argument."
-            exit 2
-          fi
-          ${./util/test_runner.py} -t 30 fpga "$1" \
-            --elf-file ${sonata-system-software}/bin/test_runner \
-            --tcl-file ${./util/sonata-openocd-cfg.tcl}
-          ${./util/test_runner.py} -t 600 fpga "$1" \
-            --uf2-file ${cheriot-rtos-test-suite}/share/test-suite.uf2
-        '';
-      };
-
-      tests-simulator = pkgs.stdenvNoCC.mkDerivation {
-        name = "tests-simulator";
-        src = ./.;
-        dontBuild = true;
-        doCheck = true;
-        buildInputs = [sonata-simulator pythonEnv];
-        checkPhase = ''
-          python ${./util/test_runner.py} -t 60 sim \
-              --elf-file ${sonata-system-software}/bin/test_runner
-          echo "Test runner complete!"
-          python ${./util/test_runner.py} -t 600 sim \
-              --sim-boot-stub ${sonata-sim-boot-stub.out}/share/sim_boot_stub \
-              --elf-file ${cheriot-rtos-test-suite}/share/test-suite
-        '';
-        installPhase = "mkdir $out";
+      tests = import nix/tests.nix {
+        inherit
+          pkgs
+          pythonEnv
+          sonata-system-software
+          sonata-sim-boot-stub
+          cheriot-rtos-test-suite
+          sonata-simulator
+          ;
       };
 
       lint = import nix/lint.nix {
@@ -240,14 +216,14 @@
           ;
         sonata-simulator-lint = lint.sonata-simulator;
       };
-      checks = {inherit tests-simulator;};
+      checks = {test-simulator = tests.simulator;};
       apps = builtins.listToAttrs (map (program: {
         inherit (program) name;
         value = {
           type = "app";
           program = getExe program;
         };
-      }) [lint.all lint.python tests-fpga lint.cpp]);
+      }) [lint.all lint.python tests.fpga lint.cpp]);
     };
   in
     flake-utils.lib.eachDefaultSystem system_outputs;
