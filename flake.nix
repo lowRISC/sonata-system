d9cb8e3e8958859aa77690b4e1a2e152124e5308
Douglas Reis <doreis@lowrisc.org>
2024-09-25 15:41:48 +0100

    [nix] move linters to a module
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- flake.nix
+++ flake.nix
@@ -68,44 +68,6 @@
         };
       };
 
-      lint-python = pkgs.writeShellApplication {
-        name = "lint-python";
-        runtimeInputs = [pythonEnv];
-        text = ''
-          ruff format --check .
-          ruff check .
-          mypy .
-        '';
-      };
-
-      lint-all = pkgs.writers.writeBashBin "lint-all" ''
-        set -e
-        ${getExe pkgs.reuse} --suppress-deprecation lint
-        ${getExe pkgs.lychee} --offline --no-progress .
-        ${getExe lint-python}
-        ${getExe lint-cpp}
-      '';
-
-      lint-cpp = pkgs.writeShellApplication {
-        name = "lint-cpp";
-        runtimeInputs = [pkgs.clang-tools_18];
-        text = ''
-          set +u
-          EXCLUDE="sw/cheri/build"
-          FILES=$(find sw -type f -path "$EXCLUDE" -prune -o \( -name "*.c" -o -name "*.cc" -o -name "*.h" -o -name "*.hh" \))
-          ARG="$1"
-          [ -z "$1" ] && ARG="check"
-          case "$ARG" in
-            check)
-              echo "$FILES" | xargs clang-format -n --Werror
-            ;;
-            fix)
-              echo "$FILES" | xargs clang-format -i
-            ;;
-          esac
-        '';
-      };
-
       cheriotPkgs = lowrisc-nix.outputs.devShells.${system}.cheriot.nativeBuildInputs;
 
       sonataSimulatorFileset = fileset.toSource {
@@ -121,22 +83,7 @@
           ./open_hbmc.core
         ];
       };
-      sonata-simulator-lint = pkgs.stdenvNoCC.mkDerivation {
-        name = "sonata-simulator-lint";
-        src = sonataSimulatorFileset;
-        buildInputs = with pkgs; [libelf zlib];
-        nativeBuildInputs = [pkgs.verilator pythonEnv];
-        inherit FLAKE_GIT_COMMIT;
-        inherit FLAKE_GIT_DIRTY;
-        dontBuild = true;
-        doCheck = true;
-        checkPhase = ''
-          HOME=$TMPDIR fusesoc --cores-root=. run \
-            --target=lint --setup --build lowrisc:sonata:system \
-            --verilator_options="+define+RVFI -j $NIX_BUILD_CORES"
-        '';
-        installPhase = "mkdir $out";
-      };
+
       sonata-simulator = pkgs.stdenv.mkDerivation rec {
         inherit version;
         pname = "sonata-simulator";
@@ -252,6 +199,16 @@
         '';
         installPhase = "mkdir $out";
       };
+
+      lint = import nix/lint.nix {
+        inherit
+          pkgs
+          pythonEnv
+          sonataSimulatorFileset
+          FLAKE_GIT_COMMIT
+          FLAKE_GIT_DIRTY
+          ;
+      };
     in {
       formatter = pkgs.alejandra;
       devShells.default = pkgs.mkShell {
@@ -276,12 +233,12 @@
       packages = {
         inherit
           sonata-simulator
-          sonata-simulator-lint
           sonata-sim-boot-stub
           sonata-documentation
           sonata-system-software
           cheriot-rtos-test-suite
           ;
+        sonata-simulator-lint = lint.sonata-simulator;
       };
       checks = {inherit tests-simulator;};
       apps = builtins.listToAttrs (map (program: {
@@ -290,7 +247,7 @@
           type = "app";
           program = getExe program;
         };
-      }) [lint-all lint-python tests-fpga lint-cpp]);
+      }) [lint.all lint.python tests-fpga lint.cpp]);
     };
   in
     flake-utils.lib.eachDefaultSystem system_outputs;
