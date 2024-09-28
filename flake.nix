4209cb72ceea7a25187b37ad6a649eed7ee53ad6
Alex Jones <alex.jones@lowrisc.org>
2024-09-28 01:22:38 +0100

    Re-add the CHERIoT RTOS test suite to Nix testing
    This commit re-adds the CHERIoT RTOS test suite to the simulator and
    FPGA test suites, effectively reverting commit `4171621`. After RTL
    changes and moving the CHERIoT RTOS revision over to using our
    local hyperram hack, the CHERIoT RTOS test suite now passes on
    `sonata-system` again.

--- flake.nix
+++ flake.nix
@@ -219,6 +219,8 @@
           ${./util/test_runner.py} -t 30 fpga "$1" \
             --elf-file ${sonata-system-software}/bin/test_runner \
             --tcl-file ${./util/sonata-openocd-cfg.tcl}
+          ${./util/test_runner.py} -t 600 fpga "$1" \
+            --uf2-file ${cheriot-rtos-test-suite}/share/test-suite.uf2
         '';
       };
 
@@ -231,6 +233,9 @@
         checkPhase = ''
           python ${./util/test_runner.py} -t 60 sim \
               --elf-file ${sonata-system-software}/bin/test_runner
+          python ${./util/test_runner.py} -t 600 sim \
+              --sim-boot-stub ${sonata-sim-boot-stub.out}/share/sim_boot_stub \
+              --elf-file ${cheriot-rtos-test-suite}/share/test-suite
         '';
         installPhase = "mkdir $out";
       };
