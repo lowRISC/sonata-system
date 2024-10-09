ecbf7f88459a84ffac416c77796c51973550d9f5
Adrian Lees <a.lees@lowrisc.org>
2024-10-09 21:59:24 +0100

    Patched TL-UL files for Sonata use
    Support the removal of all integrity signalling because this
    is not required within the Sonata FPGA, consumes resources and
    leads in places to UNOPTFLAT warnings from Verilator.

--- vendor/lowrisc_ip/ip/tlul/rtl/tlul_cmd_intg_gen.sv
+++ vendor/lowrisc_ip/ip/tlul/rtl/tlul_cmd_intg_gen.sv
@@ -9,6 +9,7 @@
  */
 
 module tlul_cmd_intg_gen import tlul_pkg::*; #(
+  parameter bit EnableCmdIntgGen = 1'b1,
   parameter bit EnableDataIntgGen = 1'b1
 ) (
   // TL-UL interface
@@ -16,15 +17,20 @@ module tlul_cmd_intg_gen import tlul_pkg::*; #(
   output tl_h2d_t tl_o
 );
 
-  tl_h2d_cmd_intg_t cmd;
-  assign cmd = extract_h2d_cmd_intg(tl_i);
-  logic [H2DCmdMaxWidth-1:0] unused_cmd_payload;
-
   logic [H2DCmdIntgWidth-1:0] cmd_intg;
-  prim_secded_inv_64_57_enc u_cmd_gen (
-    .data_i(H2DCmdMaxWidth'(cmd)),
-    .data_o({cmd_intg, unused_cmd_payload})
-  );
+
+  if (EnableCmdIntgGen) begin : gen_cmd_intg
+    tl_h2d_cmd_intg_t cmd;
+    assign cmd = extract_h2d_cmd_intg(tl_i);
+    logic [H2DCmdMaxWidth-1:0] unused_cmd_payload;
+
+    prim_secded_inv_64_57_enc u_cmd_gen (
+      .data_i(H2DCmdMaxWidth'(cmd)),
+      .data_o({cmd_intg, unused_cmd_payload})
+    );
+  end else begin : gen_no_cmd_intg
+    assign cmd_intg = tl_i.a_user.cmd_intg;
+  end
 
   logic [top_pkg::TL_DW-1:0] data_final;
   logic [DataIntgWidth-1:0] data_intg;
--- vendor/lowrisc_ip/ip/tlul/rtl/tlul_rsp_intg_gen.sv
+++ vendor/lowrisc_ip/ip/tlul/rtl/tlul_rsp_intg_gen.sv
@@ -17,8 +17,8 @@ module tlul_rsp_intg_gen import tlul_pkg::*; #(
   output tl_d2h_t tl_o
 );
 
-  logic [D2HRspIntgWidth-1:0] rsp_intg;
   if (EnableRspIntgGen) begin : gen_rsp_intg
+    logic [D2HRspIntgWidth-1:0] rsp_intg;
     tl_d2h_rsp_intg_t rsp;
     logic [D2HRspMaxWidth-1:0] unused_payload;
 
@@ -28,30 +28,29 @@ module tlul_rsp_intg_gen import tlul_pkg::*; #(
       .data_i(D2HRspMaxWidth'(rsp)),
       .data_o({rsp_intg, unused_payload})
     );
+
+    logic [DataIntgWidth-1:0] data_intg;
+    if (EnableDataIntgGen) begin : gen_data_intg
+      logic [DataMaxWidth-1:0] unused_data;
+      tlul_data_integ_enc u_tlul_data_integ_enc (
+        .data_i(DataMaxWidth'(tl_i.d_data)),
+        .data_intg_o({data_intg, unused_data})
+      );
+    end else begin : gen_passthrough_data_intg
+      assign data_intg = tl_i.d_user.data_intg;
+    end
+
+    always_comb begin
+      tl_o = tl_i;
+      tl_o.d_user.rsp_intg = rsp_intg;
+      tl_o.d_user.data_intg = data_intg;
+    end
   end else begin : gen_passthrough_rsp_intg
-    assign rsp_intg = tl_i.d_user.rsp_intg;
-  end
-
-  logic [DataIntgWidth-1:0] data_intg;
-  if (EnableDataIntgGen) begin : gen_data_intg
-    logic [DataMaxWidth-1:0] unused_data;
-    tlul_data_integ_enc u_tlul_data_integ_enc (
-      .data_i(DataMaxWidth'(tl_i.d_data)),
-      .data_intg_o({data_intg, unused_data})
-    );
-  end else begin : gen_passthrough_data_intg
-    assign data_intg = tl_i.d_user.data_intg;
-  end
-
-  always_comb begin
-    tl_o = tl_i;
-    tl_o.d_user.rsp_intg = rsp_intg;
-    tl_o.d_user.data_intg = data_intg;
+    assign tl_o = tl_i;
   end
 
   logic unused_tl;
   assign unused_tl = ^tl_i;
-
 
   `ASSERT_INIT(PayLoadWidthCheck, $bits(tl_d2h_rsp_intg_t) <= D2HRspMaxWidth)
   `ASSERT_INIT(DataWidthCheck_A, $bits(tl_i.d_data) <= DataMaxWidth)
