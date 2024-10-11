c814bad026a7532d7f819dea2e332d0f1e937164
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-10-11 11:37:02 +0100

    Script to run all regtool commands

---
+++ util/reg_gen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+declare -a regtoolblocks=("rgbled_ctrl" "spi" "system_info")
+
+for block in "${regtoolblocks[@]}"
+do
+  vendor/lowrisc_ip/util/regtool.py -r -t rtl/ip/"$block"/rtl rtl/ip/"$block"/data/"$block".hjson
+done
