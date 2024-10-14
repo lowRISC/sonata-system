79bef56a27d89e48fab67559f7f33366d3ae5770
Douglas Reis <doreis@lowrisc.org>
2024-10-14 11:58:35 +0100

    [ci, fpga] Disable cherirtos testsuite for now
    We can't use the uf2 flow in ci becase the filesystem is not mounted in
    the container. We can't also load it with via JTAG because the cherirtos
    expects to run from the Software entry point rather than the ROM
    entrypoint.
    This will be fixed in a follow-up PR.
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- nix/tests.nix
+++ nix/tests.nix
@@ -19,12 +19,10 @@
           "as the first argument."
         exit 2
       fi
-
+      echo "Sonata system test suite"
       ${../util/test_runner.py} -t 30 fpga "$1" \
         --elf-file ${sonata-system-software}/bin/test_runner \
         --tcl-file ${../util/sonata-openocd-cfg.tcl}
-      ${../util/test_runner.py} -t 600 fpga "$1" \
-        --uf2-file ${cheriot-rtos-test-suite}/share/test-suite.uf2
     '';
   };
 
