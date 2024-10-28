fe432d44af302150b3e3a1c5154f7f2f370b59c2
Hugo McNally <hugo.mcnally@gmail.com>
2024-10-28 17:47:39 +0000

    top_gen: return non zero error code when subprocess fails

--- util/generate_xbar.sh
+++ util/generate_xbar.sh
@@ -1,6 +1,7 @@
 # Copyright lowRISC contributors.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
+set -e
 
 mkdir -p rtl/bus
 vendor/lowrisc_ip/util/tlgen.py -t data/xbar_main_generated.hjson -o rtl/bus
