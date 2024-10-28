0f366cbdb2501a84cb8dc8ec6fdd388ce1e5a122
Greg Chadwick <gac@lowrisc.org>
2024-10-28 15:55:08 +0000

    [rtl] Explicitly set RV32B/RV32M for Ibex
    This is effectively a no-op change. Before the latest Ibex was vendored
    we had no bitmanip (the RV32BFull parameter was not fully passed
    through) and RV32M was the fast multiplier.
    
    Sadly the single cycle multiplier seems to be increasing timing
    pressure. It does just meet timing but greatly increases synthesis
    times. As it's implemented with in-built FPGA DSP blocks it shouldn't be
    a big issue to use it so something to examine here but for now leave
    things as they are.

--- rtl/system/sonata_system.sv
+++ rtl/system/sonata_system.sv
@@ -757,7 +757,8 @@ module sonata_system
     .HeapBase        ( tl_main_pkg::ADDR_SPACE_SRAM            ),
     .TSMapBase       ( tl_main_pkg::ADDR_SPACE_REV_TAG         ),
     .TSMapSize       ( RevTagDepth                             ),
-    .RV32B           ( ibex_pkg::RV32BFull                     ),
+    .RV32M           ( ibex_pkg::RV32MFast                     ),
+    .RV32B           ( ibex_pkg::RV32BNone                     ),
     .ICache          ( 1'b1                                    )
   ) u_top_tracing (
     .clk_i                  (clk_sys_i),
