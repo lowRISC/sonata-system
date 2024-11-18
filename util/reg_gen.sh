685922f5fc93c4305a05f17f4ed354a5fba00063
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-11-18 15:09:50 +0000

    Revocation control register generated
    Including:
    - Start address
    - End address
    - Go (hardware extended)
    - Epoch
    - Interrupt status (hardware extended)
    - Interrupt enable

--- util/reg_gen.sh
+++ util/reg_gen.sh
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-declare -a regtoolblocks=("rgbled_ctrl" "spi" "system_info")
+declare -a regtoolblocks=("rgbled_ctrl" "spi" "system_info" "rev_ctl")
 
 for block in "${regtoolblocks[@]}"
 do
