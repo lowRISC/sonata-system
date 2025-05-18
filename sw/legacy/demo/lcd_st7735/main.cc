9eaf12a26891677b26a5535f7c166de4c56e768a
Douglas Reis <doreis@lowrisc.org>
2025-05-18 18:46:41 +0100

    [legacy] Convert lcd demo to C++
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/legacy/demo/lcd_st7735/main.cc
+++ sw/legacy/demo/lcd_st7735/main.cc
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+extern "C" {
 #include "core/lucida_console_10pt.h"
 #include "core/m3x6_16pt.h"
 #include "fbcon.h"
@@ -14,6 +15,9 @@
 #include "spi.h"
 #include "st7735/lcd_st7735.h"
 #include "timer.h"
+
+int coremark_main();
+}
 
 #define SIMULATION 0
 
@@ -33,6 +37,7 @@ enum {
 // Buttons
 // The direction is relative to the screen in landscape orientation.
 typedef enum {
+  NONE      = 0x00,
   BTN_DOWN  = 0b00001 << 8,
   BTN_LEFT  = 0b00010 << 8,
   BTN_CLICK = 0b00100 << 8,
@@ -40,12 +45,58 @@ typedef enum {
   BTN_UP    = 0b10000 << 8,
 } Buttons_t;
 
-// Local functions declaration.
-static uint32_t spi_write(void *handle, uint8_t *data, size_t len);
-static uint32_t gpio_write(void *handle, bool cs, bool dc);
-static void timer_delay(uint32_t ms);
 static void fractal_test(St7735Context *lcd);
-static Buttons_t scan_buttons(uint32_t timeout);
+
+// Local functions declaration.
+static uint32_t spi_write(void *handle, uint8_t *data, size_t len) {
+  spi_tx((spi_t *)handle, data, len);
+  spi_wait_idle((spi_t *)handle);
+  return len;
+}
+
+static uint32_t gpio_write(void *handle, bool cs, bool dc) {
+  spi_set_cs((spi_t *)handle, LcdDcLine, dc);
+  spi_set_cs((spi_t *)handle, LcdCsLine, cs);
+  return 0;
+}
+
+static void timer_delay(uint32_t ms) {
+#if !SIMULATION
+  // Configure timer to trigger every 1 ms
+  timer_enable(SYSCLK_FREQ / 1000);
+  uint32_t timeout = get_elapsed_time() + ms;
+  while (get_elapsed_time() < timeout) {
+    asm volatile("wfi");
+  }
+  timer_disable();
+#endif
+}
+
+static Buttons_t scan_buttons(uint32_t timeout) {
+  while (true) {
+    // Sample navigation buttons (debounced).
+    uint32_t in_val = read_gpio(GPIO_IN_DBNC) & (0x1f << 8);
+    if (in_val == 0) {
+      // No button pressed, so delay for 20ms and then try again, unless the timeout is reached.
+      const uint32_t poll_delay = 20;
+      timer_delay(poll_delay);
+      if (timeout < poll_delay) {
+        // Timeout reached, return 0.
+        return static_cast<Buttons_t>(0);
+      } else {
+        // Timeout not reached yet, decrease it and try again.
+        timeout -= poll_delay;
+      }
+      continue;
+    }
+
+    // Some button pressed.
+    // Wait until the button is released to avoid an event being triggered multiple times.
+    while (read_gpio(GPIO_IN_DBNC) & in_val);
+
+    return static_cast<Buttons_t>(in_val);
+  }
+}
 
 int main(void) {
   timer_init();
@@ -101,11 +152,11 @@ int main(void) {
   };
   Menu_t main_menu = {
       .title          = "Main menu",
-      .color          = BGRColorBlue,
-      .selected_color = BGRColorRed,
-      .background     = BGRColorWhite,
-      .items_count    = sizeof(items) / sizeof(items[0]),
       .items          = items,
+      .items_count    = sizeof(items) / sizeof(items[0]),
+      .color          = BGRColorBlue,
+      .background     = BGRColorWhite,
+      .selected_color = BGRColorRed,
   };
 
   bool repaint    = true;
@@ -196,7 +247,6 @@ boot:
       lcd_println(&lcd, "CoreMark", alined_center, (LCD_Point){.x = 0, .y = 1});
       lcd_st7735_set_font_colors(&lcd, BGRColorWhite, BGRColorBlue);
 
-      int coremark_main();
       coremark_main();
       break;
   }
@@ -211,65 +261,9 @@ boot:
   return 0;
 }
 
-static Buttons_t scan_buttons(uint32_t timeout) {
-  while (true) {
-    // Sample navigation buttons (debounced).
-    uint32_t in_val = read_gpio(GPIO_IN_DBNC) & (0x1f << 8);
-    if (in_val == 0) {
-      // No button pressed, so delay for 20ms and then try again, unless the timeout is reached.
-      const uint32_t poll_delay = 20;
-      timer_delay(poll_delay);
-      if (timeout < poll_delay) {
-        // Timeout reached, return 0.
-        return 0;
-      } else {
-        // Timeout not reached yet, decrease it and try again.
-        timeout -= poll_delay;
-      }
-      continue;
-    }
-
-    // Some button pressed.
-    // Find the most significant bit set.
-    in_val |= in_val >> 1;
-    in_val |= in_val >> 2;
-    in_val |= in_val >> 4;
-    in_val = ((in_val >> 1) & (0x1f << 8)) + 1;
-
-    // Wait until the button is released to avoid an event being triggered multiple times.
-    while (read_gpio(GPIO_IN_DBNC) & in_val);
-
-    return in_val;
-  }
-}
-
 static void fractal_test(St7735Context *lcd) {
   fractal_mandelbrot_float(lcd);
   timer_delay(5000);
   fractal_mandelbrot_fixed(lcd);
   timer_delay(5000);
-}
-
-static uint32_t spi_write(void *handle, uint8_t *data, size_t len) {
-  spi_tx((spi_t *)handle, data, len);
-  spi_wait_idle((spi_t *)handle);
-  return len;
-}
-
-static uint32_t gpio_write(void *handle, bool cs, bool dc) {
-  spi_set_cs((spi_t *)handle, LcdDcLine, dc);
-  spi_set_cs((spi_t *)handle, LcdCsLine, cs);
-  return 0;
-}
-
-static void timer_delay(uint32_t ms) {
-#if !SIMULATION
-  // Configure timer to trigger every 1 ms
-  timer_enable(SYSCLK_FREQ / 1000);
-  uint32_t timeout = get_elapsed_time() + ms;
-  while (get_elapsed_time() < timeout) {
-    asm volatile("wfi");
-  }
-  timer_disable();
-#endif
 }
