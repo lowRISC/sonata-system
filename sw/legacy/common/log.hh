2678ef44b01b3016ebd6ff266f0d753cca25ee37
Douglas Reis <doreis@lowrisc.org>
2025-06-01 17:18:48 +0100

    [legacy, log] use the hal uart for log
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/legacy/common/log.hh
+++ sw/legacy/common/log.hh
@@ -2,24 +2,22 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#pragma once
 #include "fmt.hh"
 
-extern "C" {
-#include "sonata_system.h"
-}
+#include "../hal/uart.hh"
+#include "platform.hh"
 
 struct WriteUart {
-  uart_t uart;
-  WriteUart(uart_t uart) : uart(uart) { uart_init(DEFAULT_UART); }
-
+  Uart serial{platform::Uart::Uart0, 40000000};
   void write(const char* buf, size_t n) {
     while (n--) {
-      uart_out(uart, *buf++);
+      serial.write_byte(*buf++);
     }
   }
 };
 
 using Log = reisfmt::Fmt<WriteUart>;
 
-WriteUart uart{DEFAULT_UART};
+WriteUart uart;
 Log log(uart);
