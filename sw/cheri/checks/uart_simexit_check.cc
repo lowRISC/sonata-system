15a2080e6e915bd281bfee40445fe0614397b7f0
Marno van der Maas <mvdmaas+git@lowrisc.org>
2025-06-09 15:57:03 +0100

    Simulation exit: test improved
    The simulator should still exit if the first character is repeated, this
    was not the case earlier so adding this to the check.

--- sw/cheri/checks/uart_simexit_check.cc
+++ sw/cheri/checks/uart_simexit_check.cc
@@ -33,7 +33,7 @@ extern "C" [[noreturn]] void entry_point(void* rwRoot) {
 
   uart->init(BAUD_RATE);
   write_str(uart, "Trying out simulation exit!\r\n");
-  write_str(uart, "Safe to exit simulator.\xd8\xaf\xfb\xa0\xc7\xe1\xa9\xd7");
+  write_str(uart, "SSafe to exit simulator.\xd8\xaf\xfb\xa0\xc7\xe1\xa9\xd7");
   write_str(uart, "This should not be printed.\r\n");
   write_str(uart, "FAIL\r\n");
   while (true);
