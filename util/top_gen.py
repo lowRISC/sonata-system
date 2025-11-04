7cdfa13b86ae08817f9f8e2a959fb35a4002f5f1
Douglas Reis <doreis@lowrisc.org>
2025-11-04 17:38:00 +0000

    [rdl] Process the rdl files to generate templates and rtl

--- util/top_gen.py
+++ util/top_gen.py
@@ -3,10 +3,22 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+import subprocess
 from pathlib import Path
 
 from top_gen import generator, parser
 
+RDL_GEN_FOLDER = Path("build/rdl")
+
 if __name__ == "__main__":
-    config = parser.parse_top_config(Path("data/top_config.toml"))
+    # Generates reg2hw, hw2reg and rdl.json from the rdl files.
+    RDL_GEN_FOLDER.mkdir(parents=True, exist_ok=True)
+    subprocess.run(
+        ["rdl2ot", "export-rtl", "--soc", "data/top.rdl", RDL_GEN_FOLDER],
+        check=True,
+    )
+
+    config = parser.parse_top_config(
+        Path("data/top_config.toml"), RDL_GEN_FOLDER / "rdl.json"
+    )
     generator.generate_top(config)
